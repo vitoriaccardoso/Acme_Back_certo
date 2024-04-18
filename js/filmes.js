08d307329072f5318617d9f348d28f4d27cc130d
VITÓRIA CARDOSO COSTA <23111831@CFP127ALUNO.LOCAL>
2024-04-18 16:06:59 -0300

    em andamento

---
+++ js/filmes.js
@@ -0,0 +1,58 @@
+
+export async function getFilmes() {
+    const url = 'http://localhost:8080/v2/acmefilmes/filmes';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.filmes;
+}
+
+export async function getFilmeId(id) {
+    const url = `http://localhost:8080/v2/acmefilmes/filme/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.filme[0];
+}
+
+export async function postFilme(filme) {
+    const url = 'http://localhost:8080/v2/acmefilmes/filme';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(filme)
+    };
+    const response = await fetch(url, options);
+    return response.ok;
+}
+
+export async function putFilme(filme) {
+    const url = `http://localhost:8080/v2/acmefilmes/filme/${filme.id}`;
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(filme)
+    };
+    const response = await fetch(url, options);
+    return response.ok;
+}
+
+export async function deleteFilme(id){
+    try{
+        await fetch(`http://localhost:8080/v2/acmeFilmes/filme/${id}`,{
+            method: 'DELETE'
+        })
+        console.log("Filme excluído com sucesso")
+    } catch (error){
+        console.error('Erro ao excluir filme: ',error);
+    }
+}
+
+export async function selectNameFilmes(name) {
+    const url = `http://localhost:8080/v2/acmefilmes/filmes?nome=${name}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.filmes;
+}
---
+++ js/main.js
@@ -0,0 +1,96 @@
+'use strict'
+import {getFilmes, postFilme, selectNameFilmes, getFilmeId} from "./filmes.js"
+
+console.table(await getFilmes())
+
+
+const search = new URLSearchParams(window.location.search).get('search')
+const searchBar = document.getElementById('searchBar')
+const container = document.getElementById('container')
+
+const searchButton = document.getElementById('searchButton')
+searchButton.addEventListener('click', pesquisar)
+async function pesquisar(){
+    const pesquisaFilme = await selectNameFilmes(searchBar.value)
+    const listaFilmes = pesquisaFilme.filmes
+    apagarListaFilmes()
+
+    listaFilmes.forEach(element => {
+        console.log(element);
+        criarCard(element)
+    });
+}
+
+searchBar.addEventListener('keypress', (event)=>{
+    if(event.key === "Enter"){
+        pesquisar()
+    }
+})
+function apagarListaFilmes(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+
+function criarCard (filme){
+    container.classList.add('gap-8')
+    const card = document.createElement('div')
+    card.classList.add('flex')
+    card.classList.add('flex-col')
+    card.classList.add('text-[#fafafa]')
+    card.classList.add('justify-center')
+    card.classList.add('cursor-pointer')
+    const titulo = document.createElement('h2')
+    titulo.classList.add('bg-[#09090b]')
+    titulo.classList.add('text-center')
+    titulo.classList.add('mr-1')
+    titulo.classList.add('w-64')
+    titulo.classList.add('border-b-4')
+    titulo.classList.add('border-x-4')
+    titulo.classList.add('border-[#fafafa]')
+    const texto = document.createElement('p')
+    texto.textContent = filme.sinopse
+    texto.classList.add('w-72')
+    const preco = document.createElement('p')
+    preco.textContent = filme.valor_unitario
+    const capa = document.createElement('img')
+    capa.src = filme.foto_capa
+    capa.classList.add('max-h-[360px]', 'min-h-[360px]')
+    capa.classList.add('w-64')
+    capa.classList.add('border-t-4')
+    capa.classList.add('border-x-4')
+    capa.classList.add('border-[#fafafa]')
+    const dataLancamento = document.createElement('p')
+    dataLancamento.textContent = filme.data_lancamento
+    card.append(capa, titulo)
+    container.appendChild(card)
+    card.addEventListener('click',()=> {
+        window.location.href='../sobre.html?id='+filme.id
+    })
+}
+
+async function preencherContainer(){
+    const container = document.querySelector('body')
+
+    const filmes = await getFilmes()
+
+    filmes.forEach(filme => {
+        criarCard(filme)
+    })
+}
+
+// const input = document.getElementById('input')
+
+// input.addEventListener('keyup', async(nome)=>{
+//     if(input == nome){
+//         const nomeFilme = await selectNameFilmes()
+//          nomeFilme = preencherContainer()
+//     }
+// })
+
+
+
+
+preencherContainer()
